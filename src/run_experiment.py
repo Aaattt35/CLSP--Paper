fe927f8673e27492be43caaaed33c67caaf0f27a
A.T <277269226+Aaattt35@users.noreply.github.com>
2026-04-27 16:43:23 +0330

    Update run_experiment.py

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -8,6 +8,16 @@ import os
 import time
 from pyomo.environ import SolverFactory, value, TerminationCondition
 from Read_input_txt_file import Read_input  # new: import the solver function
+from build_AGG_model import build_m_Agg
+from build_FAL_model import build_Fal
+from build_SHP_model import build_Shp
+from build_TPM-m_model import build_TPM_m
+from build_TPM_model import build_Tpm
+from build_dp_solver import DP_CLSP
+from build_envl_model import build_Agg_envl
+from build_weakenvl_model import build_Agg_weakenvl
+from build_weakl_model import build_Agg_weakl
+
 
 def read_instance(path):
     data = {}
