b6058cc95ed685a1cd7e027dd1ecc3b4d5be72ca
A.T <277269226+Aaattt35@users.noreply.github.com>
2026-04-29 02:53:27 +0330

    Update ALCP_Algorithm.py

--- src/ALCP_Algorithm.py
+++ src/ALCP_Algorithm.py
@@ -1,99 +1,104 @@
 from timeit import default_timer as timer 
 
-def d_cum(a,b):
-    if b > T:
-        b = T
-    c = sum(d[k] for k in range(a, b))
-    return c
+###ALCP algorithm  
 
+def _ALCP_sgg_m()
 
-
-def q_Operator(period, start_point, jump_step, time_h):
-    t = period
-    s_p = start_point #s_p = q_tplas1 +1 
-    j_s = jump_step
-    T = time_h
-    e_p = T
-    r_cap = cap[t] - d[t]
-    
-    if r_cap - d_cum(s_p, s_p + j_s) < 0 and s_p + j_s < T + 2:
-        while j_s >= 1 and (r_cap - d_cum(s_p, s_p + j_s - 1) < 0):
-            j_s -= 1
-        e_p = s_p + j_s - 1
-
-    if r_cap - d_cum(s_p, s_p + j_s) >= 0 and s_p + j_s < T + 2:
-        while j_s < T - s_p and (r_cap - d_cum(s_p, s_p + j_s + 1) > 0):
-            j_s += 1
-        e_p = s_p + j_s 
-    return e_p
-
-def q_C_k_Opr(period, R_cap, start_point, jump_step, time_h):
-    t = period
-    s_p = start_point #s_p = q_tplas1 +1 
-    j_s = jump_step
-    T = time_h
-    e_p = T
-    r_cap = R_cap
-    if r_cap - d_cum(s_p, s_p + j_s) < 0 and s_p + j_s < T + 2:
-        while j_s >= 1 and (r_cap - d_cum(s_p, s_p + j_s - 1) < 0):
-            j_s -= 1
-        e_p = s_p + j_s - 1
-
-    if r_cap - d_cum(s_p, s_p + j_s) >= 0 and s_p + j_s < T + 2:
-        while j_s < T - s_p and (r_cap - d_cum(s_p, s_p + j_s + 1) > 0):
-            j_s += 1
-        e_p = s_p + j_s 
-    return e_p
-
-
-#### upper bound m suggested by ALSP algorithm 
-def ALCP_sgg_m(T, cap, d, c):      #ALCP suggestion for m 
     start = timer()
-    cap = cap
-    T = T
-    c = c
-    d = d
     m_hat = {t: T - t + 1 for t in range(1, T+1)}
+    m_hat_sen_2 = {t: T - t + 1 for t in range(1, T+1)}
     q_r_star = {t: T for t in range(1,T+1)}
-    r_star = {t: t for t in range(1,T+1)}
-    q_tplas1 = {t: T for t in range(1,T+1)}
+    r_star = {t: T for t in range(1,T+1)}
+    q_tplas1_t = {t: T for t in range(1,T+1)}
+
     
+    def d_cum(a,b):
+        if b > T:
+            b = T
+        c = sum(d[k] for k in range(a, b))
+        return c
+
+
+    
+    def q_Operator(period, R_cap, start_point, jump_step, time_h):
+        t = period
+        s_p = start_point #s_p: start point = q_tplas1 +1 
+        j_s = jump_step
+        T = time_h
+        e_p = T + 1  # e_p: ending point
+        r_cap = R_cap
+        rim_cap = r_cap - d_cum(s_p, s_p + j_s) #remaind capacity if period t produced for its own and peirods in [s_p, s_p + j_s - 1]
+        if rim_cap < 0 and s_p + j_s < T + 2:
+            while j_s >= 1 and (r_cap - d_cum(s_p, s_p + j_s - 1) < 0):
+                j_s -= 1
+            e_p = s_p + j_s - 1
+
+        if rim_cap >= 0 and s_p + j_s < T + 2:
+            while j_s < T - s_p and (r_cap - d_cum(s_p, s_p + j_s + 1) > 0):
+                j_s += 1
+            e_p = s_p + j_s 
+        return e_p - 1
+
+    
+
+    def _Delta_(period_a, period_b, period_c, period_d):
+        t = period_a
+        r = period_b
+        q_r_t = period_c 
+        q_rplus1_r = period_d
+        if (q_r_t < T and q_rplus1_r < T):
+            delta = sum(a[(t, k)] for k in range(r, q_r_t + 1)) + int(((cap[t] - d[t] - d_cum(r, q_r_t + 1))/d[q_r_t + 1]) * a[t, q_r_t + 1]) - sum(a[(r, k)] for k in range(r + 1, q_rplus1_r))
+            return delta
+        else:
+            return 1
+
+    ##### computig r_star(t) and q_r_star[t] = q_Operator(t, cap[t] - d[t], r_star(t), c, T)    
     for t in reversed(range(1, T - c)):
-        print(f".....................................................t = {t}")
-        q_tplas1[t] = q_Operator(t, t+1, c, T)
-        print(f"..q_tplas1_[{t}] = {q_tplas1[t]}")
-        q_r_t = q_tplas1[t]
-        print(f"r_cap({t})-D[{t+1}, {q_r_t +1}) = [{cap[t] - d[t] - d_cum(t+1, q_r_t +1)}")
-        max_r = min(T - 2, T - c + 3)
+        max_r = min(T - 2, T - c + 3)  
         for r in range(t + 2,  max_r):
-            print(f"----------r = {r}--and--q_r_t = {q_r_t}")
-            jj = 0
-            print(f"cap_rim[{r}, {q_r_t}) = [{cap[t] - d[t] - d_cum(r, q_r_t)}")
-            while jj < T - q_r_t and cap[t] - d[t] - d_cum(r, q_r_t + jj) > 0 :
-                jj += 1
-                print(f"In while :: cap_rim[{r}, {q_r_t + jj}] = [{cap[t] - d[t] - d_cum(r, q_r_t + jj)}")
-            q_r_t = q_r_t + jj
-            print(f"          cap_rim[{r}, {q_r_t+1}) = {cap[t] - d[t] - d_cum(r, q_r_t)}")
-            q_rplus1_r = q_Operator(r, r + 1, c, T)
-            if (q_r_t < T-1 and q_rplus1_r < T-1):
-                
-                print(f"..q_rplus1_{r} = {q_rplus1_r}")
-                                
-                prof = sum(a[(t, k)] for k in range(r + 1, q_r_t + 2)) + int(((cap[t] - d_o[t] - d_cum(r + 1, q_r_t + 2))/d_o[r]) * a[t, r])
-                compon1 = cap[r] - d_o[r] - d_cum(r + 2, min(q_rplus1_r + 2, T+1))
-                compon2 = a[r, q_rplus1_r + 1] / d_o[q_rplus1_r + 1]
-                max_t_benef = sum(a[(r, k)] for k in range(r + 2, min(q_rplus1_r + 2, T+1))) + int(compon1 * compon2)
-                    
-                print(f"..prof:{prof} < max_t_benef:{max_t_benef}")
-                if prof < max_t_benef:
-                    q_r_star[t] = q_r_t
-                    r_star[t] = r
-                    m_hat[t] = q_r_t - t
-                    print(f"max profit of producing in period{t} to be used in range[{r}, {q_r_t  + 1}) = {prof}")
-                    print(f"Max profit of producing in period{r} to be used in range[{r + 1}, {q_rplus1_r+1}] = {max_t_benef}")
-                    break
+            q_r_t = q_Operator(t, cap[t] - d[t], r, c, T)
+            q_rplus1_r = q_Operator(r, cap[r] - d[r], r + 1, c, T)
+            if (q_r_t == T or q_rplus1_r == T or r >= q_r_t):
+                break
+            if (_Delta_(t, r, q_r_t, q_rplus1_r) < 0):
+                q_r_star[t] = q_r_t
+                r_star[t] = r
+                m_hat_sen_2[t] = q_r_t - t #m_hat from second senario
+                break
+        #### Evaluate whether converting some non‑PPs in range(r_star[t], q_r_star[t] + 1) to PPs 
+        #### ,in the worst case, causes period e to start after r_star[e].
+        r_star_set = {k: r_star[k] for k in range(r_star[t], q_r_star[t]+1)}
+        Cap_set = {k: cap[k] for k in range(r_star[t], q_r_star[t]+1)}
+        e = max(reversed(r_star_set), key=r_star_set.get)
+        max_r_star_s = r_star_set[e]
+        if (max_r_star_s < T):
+            R_cap = cap[t] - d[t] + d[e] 
+            R_cap_0= cap[t] - d[t] + d[e]
+            del Cap_set[e] 
+            J_m = m_hat_sen_2[t] # jump using m_har of second senario
+            ml = q_Operator(t, R_cap, t + 1, J_m, T)# ml(0,0)
+            mm = 0
+            while ml < r_star_set[e] and Cap_set:    #adding cap[i] to (5), we can use ml < max_q_r_star_s
+                m_j =  min(Cap_set, key = Cap_set.get)
+                cap_min_j = Cap_set[m_j]
+                if(m_j < e):        #early periods should allocate their productions earlier()
+                    R_cap = R_cap + cap[m_j] 
+                    R_cap_0= R_cap_0 + d[m_j]
+                    del Cap_set[m_j]
+                    J_m = J_m + m_hat_sen_2[j]
+                    ml = q_Operator(t, R_cap, t, J_m, T)
+                    mm = mm + 1
+                else:
+                    del Cap_set[m_j]
+
+            ml_0 = q_Operator(t, R_cap_0, t + 1, m_hat_sen_2[t], T)
+            ap = ml_0 - q_Operator(t, cap[t] - d[t], t + 1, c, T)      
+            m_hat[t] = m_hat_sen_2[t] + ap  #= m_hat[t] + mm + 1                
+
     dur = timer() - start
-    Func_q_r_star.Time = dur
+    ALCP_sgg_m.Time = dur
+    return m_hat
+    
     
     
     
