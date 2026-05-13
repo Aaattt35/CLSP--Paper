52744af118148a30e24363dc91c5aeb9ba9aecdf
A.T <277269226+Aaattt35@users.noreply.github.com>
2026-05-13 18:30:23 +0330

    Update ALCP_Algorithm.py

--- src/ALCP_Algorithm.py
+++ src/ALCP_Algorithm.py
@@ -1,6 +1,8 @@
-from timeit import default_timer as timer 
+### ALCP.4.0.V: WITHOUT print version 
+### ristricted by r_star[q_r_star[t]]
 
-###ALCP_algorithm_Final
+from itertools import product
+from timeit import default_timer as timer 
 
 def _ALCP_sgg_m(T, cap, d, c, h, p, s):
     
@@ -30,7 +32,7 @@ def _ALCP_sgg_m(T, cap, d, c, h, p, s):
                 a[(t, j)] = s[j] - (h_tj[(t, j)] - p[j] + p[t]) * d[j]
                 a_ratio[(t,j)] = (s[j] + p[j]*d[j])/((h_tj[(t, j)] + p[t]) * d[j])
 
-    
+    ## define needed functions
     def d_cum(a,b):
         if b > T:
             b = T
@@ -65,11 +67,10 @@ def _ALCP_sgg_m(T, cap, d, c, h, p, s):
         t_profit = r_profit = 0
         if (q_r_t < T and q_rplus1_r < T):
             for k in range(r, q_r_t + 1):
-                if a[t, k] > 0:
-                    t_profit = t_profit + a[t, k]  #+ int(((cap[t] - d[t] - d_cum(r, q_r_t + 1))/d[q_r_t + 1]) * a[t, q_r_t + 1]) 
-
-            for k in range(r + 1, q_rplus1_r + 1):
-                if a[r, k] > 0:
+                t_profit = t_profit + a[t, k]  
+            t_profit = t_profit+ int(((cap[t] - d[t] - d_cum(r, q_r_t + 1))/d[q_r_t + 1]) * a[t, q_r_t + 1])         
+            if q_rplus1_r > r:
+                for k in range(r + 1, q_rplus1_r + 1):
                     r_profit = r_profit + a[r, k]
             delta = t_profit - r_profit
             return delta
@@ -85,35 +86,27 @@ def _ALCP_sgg_m(T, cap, d, c, h, p, s):
             if (q_r_t == T): #or q_rplus1_r == T
                 r_str = T
                 break
-                
-            if (r < q_rplus1_r):
-                if (r <= q_r_t and _Delta_(t, r, q_r_t, q_rplus1_r) < 0 and q_Operator(t, Available_cap, r-1, c) >= r-1):
-                    r_str = r - 1
-                    break
+            if (_Delta_(t, r, q_r_t, q_rplus1_r) < 0):
+                r_str = r - 1
+                break
 
         return r_str
 
-
-    ##### computig r_star(t) and q_r_star[t]
+    
     for t in reversed(range(1, T - c)):
-        r_star[t] = _r_star_(t, cap[t] - d[t])
+        r_star[t] = _r_star_(t, cap[t] - d[t])     ##### computig r_star(t) and q_r_star[t]
         q_r_star[t] = q_Operator(t, cap[t] - d[t], r_star[t], c)
-        m_hat_sen_2[t] = q_r_star[t] - t #m_hat from second senario       
-        ##### computing m_hat_1[t] for the situation when all periods in range r_star(t) to q_r_star[t]
-        ##### has been converted to pp
-        if r_star[t] < T:
-            extra_cap = cap[t] - d[t] 
-            for new_pp in range(r_star[t],  q_r_star[t]+1):
-                extra_cap = extra_cap + cap[new_pp]
+        m_hat_sen_2[t] = q_r_star[t] - t + 1     # m_hat from second senario       
 
-            r_pp_str = _r_star_(t, extra_cap) #### provides r^* when all periods in safty range was converted to pp
-            q_r_pp_str = q_Operator(t, extra_cap, r_pp_str, c)
-            if q_r_pp_str < T :
-                m_hat_1[t] = q_r_pp_str - t  # m_hat_1[t] : this is useful just for c<5. 
-
+        ## when the coverate of period t after r*(t) is limited to one, tow or three points (even not restricted at all) 
+        ## it is obvious that period t's production should not force period q^[r*]_(t) to be started after its border line of r*_(q^[r*]_(t))
+        if r_star[t] < T-3 and q_r_star[t] - r_star[t] <= 2 and r_star[r_star[t]] < T:
+            final_point = q_r_star[t]
+            if r_star[final_point] - t + 2 < T:
+                m_hat_1[t] = r_star[final_point] - t + 1
         if q_r_star[t] - r_star[t] >= 1:
-            r_star_set = {k: r_star[k] for k in range(r_star[t], q_r_star[t]+1)}
-            Cap_set = {k: cap[k] for k in range(r_star[t], q_r_star[t]+1)}
+            r_star_set = {k: r_star[k] for k in range(r_star[t], q_r_star[t] + 1)}
+            Cap_set = {k: cap[k] for k in range(r_star[t], q_r_star[t] + 1)}
 
             e = max(reversed(r_star_set), key=r_star_set.get)
             max_r_star_s = r_star_set[e]
@@ -123,30 +116,32 @@ def _ALCP_sgg_m(T, cap, d, c, h, p, s):
                 R_cap_0= cap[t] - d[t] + d[e]
                 del Cap_set[e] 
                 J_m = m_hat_sen_2[t] # jump using m_har of second senario
-                ml = q_Operator(t, R_cap, t + 1, J_m)# ml(0,0)
+                ml = q_Operator(t, R_cap, r_star[t], J_m)# ml(0,0)
                 mm = 0
                 while ml < r_star_set[e] and Cap_set:    #adding cap[i] to (5), we can use ml < max_q_r_star_s
                     m_j =  min(Cap_set, key = Cap_set.get)
                     cap_min_j = Cap_set[m_j]
                     if(m_j < e):        #early periods should allocate their productions earlier()
                         R_cap = R_cap + cap[m_j] 
-                        R_cap_0= R_cap_0 + d[m_j]
+                        R_cap_0 = R_cap_0 + d[m_j]
                         del Cap_set[m_j]
                         J_m = J_m + m_hat_sen_2[m_j]
-                        ml = q_Operator(t, R_cap, t+1, J_m)
+                        ml = q_Operator(t, R_cap, r_star[t], J_m)
                         mm = mm + 1
                     else:
                         del Cap_set[m_j]
 
                 if ml > r_star_set[e]:
-                    ml_0 = q_Operator(t, R_cap_0, t + 1, m_hat_sen_2[t])
-                    ap = ml_0 - q_Operator(t, cap[t] - d[t], t + 1, c)      
-                    m_hat[t] = m_hat_sen_2[t] + ap  #= m_hat[t] + mm + 1                
+                    ml_0 = q_Operator(t, R_cap_0, r_star[t], m_hat_sen_2[t])
+                    ap = ml_0 - q_r_star[t] # =q_Operator(t, cap[t] - d[t], t + 1, c)      
+                    m_hat[t] = m_hat_sen_2[t] + ap + 1  #or = ml_0 - t                
 
         m_hat[t] = min(m_hat[t], m_hat_1[t])
 
     dur = timer() - start
     _ALCP_sgg_m.Time = dur
 
+
     return m_hat
+
     
